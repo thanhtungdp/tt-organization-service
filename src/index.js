a55300246ee6d33191c29452874980d1aafdcfde
thanhtungdp <tungptkh@gmail.com>
2018-07-16 16:56:57 +0700

    update fixcode

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ const { router, get } = require('microrouter')
 const { handleErrors } = require('@bit/tungtung.micro.components.micro-boom')
 
 module.exports = router(
-  get(
-    '/test',
-    handleErrors(
-      (req, res) => 'Welcome clean service'
-    )
-  ),
+  get('/test', handleErrors((req, res) => 'Welcome clean service')),
   get('/health', () => 'Working...')
 )
