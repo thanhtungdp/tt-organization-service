6471bee2cd025ed019f7fd17808e2b48c3d22a10
thanhtungdp <tungptkh@gmail.com>
2018-07-20 16:51:57 +0700

    example or service

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  serviceName: 'clean',
+  serviceName: 'organization',
   mongodbUrl: process.env.MONGODB_URL
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const { router, get, withNamespace } = require('microrouter')
 const { handleErrors } = require('@bit/tungtung.micro.components.micro-boom')
 const config = require('config')
-const faker = require('faker')
 
 const namespace = withNamespace(`/${config.serviceName}`)
 
 module.exports = router(
   namespace(
-    get('/fake', () => {
-      return faker.name.findName()
-    }),
-    get('/test', handleErrors(() => 'Welcome clean service')),
+    get(
+      '/:slug',
+      handleErrors(req => ({
+        name: req.params.slug,
+        slug: req.params.slug,
+        dbInfo:
+          'mongodb://' + process.env.NODE_ENV === 'docker'
+            ? 'mongo'
+            : 'localhost' + ':27017/micro-user'
+      }))
+    ),
     get('/*', () => config.serviceName)
   ),
   get('/health', () => 'Working...'),
